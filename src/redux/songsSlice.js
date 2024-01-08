51820c6a14a17d978d84301598da9dc120198587
meronabebe <meriabebehab@gmail.com>
2024-01-07 23:44:28 -0700

    Update code

--- src/redux/songsSlice.js
+++ src/redux/songsSlice.js
@@ -1,25 +1,25 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const apiUrl = 'https://jsonplaceholder.typicode.com';
+const apiUrl = 'https://jsonplaceholder.typicode.com'; // Replace with your backend server URL
 
 export const fetchSongs = createAsyncThunk('songs/fetchSongs', async () => {
-  const response = await axios.get(`${apiUrl}/posts`);
+  const response = await axios.get(`${apiUrl}/songs`);
   return response.data;
 });
 
 export const addSong = createAsyncThunk('songs/addSong', async ({ title }) => {
-  const response = await axios.post(`${apiUrl}/posts`, { title });
+  const response = await axios.post(`${apiUrl}/songs`, { title });
   return response.data;
 });
 
 export const updateSong = createAsyncThunk('songs/updateSong', async ({ id, title }) => {
-  const response = await axios.put(`${apiUrl}/posts/${id}`, { title });
+  const response = await axios.put(`${apiUrl}/songs/${id}`, { title });
   return response.data;
 });
 
 export const deleteSong = createAsyncThunk('songs/deleteSong', async (id) => {
-  await axios.delete(`${apiUrl}/posts/${id}`);
+  await axios.delete(`${apiUrl}/songs/${id}`);
   return id;
 });
 
@@ -40,6 +40,7 @@ const songsSlice = createSlice({
         const song = state.find((song) => song.id === id);
         if (song) {
           song.title = title;
+
         }
       })
       .addCase(deleteSong.fulfilled, (state, action) => {
